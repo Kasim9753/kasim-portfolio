31eb06ff9d7eb582436e7dc70eecd826a7aba8d7
Kasim9753 <kasimshekh560@gmail.com>
2026-01-29 19:49:38 +0530

    Refactor Message model export for clarity

--- models/Message.js
+++ models/Message.js
@@ -10,4 +10,6 @@ const messageSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Message", messageSchema);
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
